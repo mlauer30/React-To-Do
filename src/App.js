90c891f228ff89ec4b61b5baff92bf9b22108ca4
mlauer30 <mlauer07@gmail.com>
2022-02-03 13:29:07 -0500

    step one: Reading to-do items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
 function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  return (
+    // ...
+    <div className="app">
+    <div className="todo-list">
+      {todos.map((todo, index) => (
+        <Todo
+          key={index}
+          index={index}
+          todo={todo}
+        />
+      ))}
+    </div>
+  </div>
+  );
+}
+
+export default App;
+
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +56,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
-export default App;
+//export default App;
